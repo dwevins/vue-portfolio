896e4523f3f6ee0db21a67ff4ba86c0c99d70cec
Woody Evins <woody.evins@gmail.com>
2017-07-30 18:58:40 -0500

    add project vue component

---
+++ src/components/Project.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="project">
+        <p>project</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'project',
+    };
+</script>
+
+<style lang="css">
+</style>
