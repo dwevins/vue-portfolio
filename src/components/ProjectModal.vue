3783046fc01619e4cf9d95eaed122d49ec717269
Woody Evins <woody.evins@gmail.com>
2017-07-30 18:58:50 -0500

    add projectModal vue component

---
+++ src/components/ProjectModal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="project-modal">
+        <p>project modal</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'project-modal',
+    };
+</script>
+
+<style lang="css">
+</style>
