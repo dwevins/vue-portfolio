8d20afe876f4cc430a105b8319d85eb05e679c2e
Woody Evins <woody.evins@gmail.com>
2017-07-30 19:00:16 -0500

    use new components, linting rules

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+    <div id="app">
+        <project v-for="project in projectList"></project>
+        <project-modal></project-modal>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+    import Project from './components/Project';
+    import ProjectModal from './components/ProjectModal';
+    import ProjectList from './projects';
 
-export default {
-  name: 'app',
-  components: {
-    Hello,
-  },
-};
+    export default {
+        name: 'app',
+
+        components: {
+            Project,
+            ProjectModal,
+        },
+
+        data() {
+            return {
+                projectList: ProjectList.projects,
+                currentProject: null,
+            };
+        },
+
+        methods: {
+            openProject(id) {
+                this.currentProject = this.projectList[id];
+            },
+        },
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
