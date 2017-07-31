513f3aa51e6c0c5327eb0037197271163c5e9fab
Woody Evins <woody.evins@gmail.com>
2017-07-30 20:15:10 -0500

    project components show project info

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,14 +1,29 @@
 <template>
-    <div class="project">
-        <p>project</p>
+    <div class="project" v-on:click="openProject">
+        <p>{{ project.name }}</p>
     </div>
 </template>
 
 <script>
     export default {
         name: 'project',
+
+        props: [
+            'project',
+            'index',
+        ],
+
+        methods: {
+            openProject() {
+                this.$emit('openProject', this.index);
+            },
+        },
     };
 </script>
 
 <style lang="css">
+    .project {
+        width: 20%;
+        border: 1px solid black;
+    }
 </style>
